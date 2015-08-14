5adb1e02e26c1d6a0905b3ce732b4b3e4b965a5e
michal-stuglik <stuglik@gmail.com>
2015-08-14 07:50:24 +0200

    - features tests

--- blastplus/tests/tests.py
+++ blastplus/tests/tests.py
@@ -33,14 +33,27 @@ class UtilsTestCase(TestCase):
 class FeatureRecordTestCase(TestCase):
     def setUp(self):
         self.hsp = record.Hsp(
-            **dict(sbjct_end=203, sbjct=u'TTTGGAGCCTGAGCAGGAA', bits=35.5503, frame=(1, -1),
+            **dict(sbjct_end=203, sbjct=u'TTTGGAGCCTGAGCAGGA', bits=35.5503, frame=(1, -1),
                    query_end=19, score=38.0, gaps=0, expect=0.0122843, str='Score 38 (35 bits), expectation 1.2e-02',
-                   positives=19, sbjct_start=221, query=u'TTTGGAGCCTGAGCAGGAA',
+                   positives=19, sbjct_start=221, query=u'TTTGGAGCCTGAGCAGGA',
                    align_length=19, num_alignments=None, identities=19,
-                   query_start=1, strand=(None, None), match=u'|||||||||||||||||||'))
+                   query_start=1, strand=(None, None), match=u'||||||||||||||||||'))
+        self.limit_length = 9
+
+        self.alignment = record.Alignment(
+            **dict(hit_def='hit_def', title='title', length=self.hsp.align_length))
+        self.alignment.hsp_list.append(self.hsp)
 
     def tearDown(self):
         del self.hsp
+        del self.alignment
 
     def test_hsp(self):
         self.assertIsInstance(self.hsp, record.Hsp)
+        self.assertTrue(self.hsp.chop_sequence("AACC", 2), ['AA', 'CC'])
+        self.assertTrue(self.hsp.chop_query(), ['TTTGGAGCC', 'TGAGCAGGA'])
+
+    def test_alignment(self):
+        self.assertIsInstance(self.alignment, record.Alignment)
+        self.assertEqual(self.alignment.best_evalue(), self.hsp.expect)
+        self.assertEqual(self.alignment.best_identities(), 100.0)
