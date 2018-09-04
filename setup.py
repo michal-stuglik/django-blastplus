68ba6786beefd9777bad4c3b4ca45d968d49d3bc
Michal Stuglik <stuglik@gmail.com>
2018-09-04 17:49:02 +0200

    Update version

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
     name='django-blastplus',
-    version='2.0.1',
+    version='2.0.2',
     packages=['blastplus'],
     include_package_data=True,
     license='MIT License',
