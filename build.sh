8b4ec713d6a6dc659c9ccb406a02a8207a336730
Michal Stuglik <stuglik@gmail.com>
2018-10-30 13:21:57 +0100

    Update builder info

--- build.sh
+++ build.sh
@@ -4,3 +4,7 @@ python3 setup.py sdist bdist_wheel
 
 echo 'copy to repository'
 cp dist/django_blastplus-*.whl ~/Dropbox/Public/bin/
+
+# upload to repository
+# https://github.com/fhamborg/news-please/wiki/PyPI---How-to-upload-a-new-version
+# python setup.py sdist upload
